ac2b24bdc014617aa402a3867656d30f284f3c55
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2015-04-13 14:47:24 -0700

    Modified body from string to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150412000202) do
+ActiveRecord::Schema.define(version: 20150413214220) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
-    t.string   "body"
+    t.text     "body"
     t.decimal  "latitude"
     t.decimal  "longitude"
     t.datetime "created_at", null: false
