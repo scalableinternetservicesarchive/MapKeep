525f420b6144ac0190c1d6a8b11e6139e531f4b2
Tania <rawrtan@gmail.com>
2015-04-14 09:35:16 -0700

    Added foundation and some very basic html, still no real styling

--- Gemfile
+++ Gemfile
@@ -26,6 +26,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use devise to handle user authentication
 gem 'devise'
 
+# Gem for Foundation
+gem 'foundation-rails'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -1,7 +1,7 @@
-var lat = document.getElementById('note_latitude');
-var lng = document.getElementById('note_longitude');
 navigator.geolocation.getCurrentPosition(function(pos) {
     var coords = pos.coords;
+    var lat = document.getElementById('note_latitude');
+    var lng = document.getElementById('note_longitude');
     lat.value = coords.latitude;
     lng.value = coords.longitude;
 });
