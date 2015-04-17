d835b08a56f31c390fb3a738875b4dc5076d6766
Tania DePasquale <rawrtan@gmail.com>
2015-04-16 23:11:46 -0700

    Add info window and note body on map

--- app/assets/javascripts/geolocation/maps.js
+++ app/assets/javascripts/geolocation/maps.js
@@ -1,17 +1,43 @@
+var map;
+$('#create_note').click(dropPin);
+
 function initialize() {
   var mapOptions = {
       center: new google.maps.LatLng(notes[0].latitude, notes[0].longitude),
-      zoom: 8
+      zoom: 10
   };
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
+
+  map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
   for (var i = 0; i < notes.length; i++) {
+    var title = notes[i].title;
+    var infowindow = new google.maps.InfoWindow({
+      content: '<h3>' + title + '</h3>' + notes[i].body + '<br/><button class="button tiny">Edit</button>'
+    });
     var marker = new google.maps.Marker({
       position: new google.maps.LatLng(notes[i].latitude, notes[i].longitude),
       map: map,
-      title: notes[i].title,
-      body: notes[i].body
+      title: title,
+      draggable: true
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map, marker);
     });
   }
+}
+
+function dropPin() {
+  var infowindow = new google.maps.InfoWindow({
+    content: ''
+  });
+  var marker = new google.maps.Marker({
+    position: map.center,
+    map: map,
+    draggable: true,
+    animation: google.maps.Animation.DROP
+  });
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map, marker);
+  });
 }
