bfc0c597ccca35474e4dc0d500fedc6738a747d2
Tania DePasquale <rawrtan@gmail.com>
2015-04-17 10:17:58 -0700

    Notes added to user id, need to fix vulnerability

--- app/assets/javascripts/geolocation/maps.js
+++ app/assets/javascripts/geolocation/maps.js
@@ -1,7 +1,9 @@
 var MAPKEEP = MAPKEEP || {};
 
-MAPKEEP.init = function(notes) {
+MAPKEEP.init = function(notes, userId, auth) {
   MAPKEEP.notes = notes;
+  MAPKEEP.userId = userId;
+  MAPKEEP.authToken = auth;
   $('#create_note').click(MAPKEEP.dropPin);
 };
 
@@ -36,15 +38,25 @@ MAPKEEP.initMap = function() {
 };
 
 MAPKEEP.dropPin = function() {
-  var infowindow = new google.maps.InfoWindow({
-    content: ''
-  });
   var marker = new google.maps.Marker({
     position: MAPKEEP.map.center,
     map: MAPKEEP.map,
     draggable: true,
     animation: google.maps.Animation.DROP
   });
+
+  var infowindow = new google.maps.InfoWindow({
+    content:  '<form class="new_note" id="new_note" action="/notes" accept-charset="UTF-8" method="post">' +
+    '<label for="note_title">Title</label><input type="text" name="note[title]" id="note_title" /><br/>' +
+    '<label for="note_body">Body</label><textarea rows="5" name="note[body]" id="note_body"></textarea>' +
+    '<input name="note[latitude]" type="hidden" value="' + marker.position.lat() + '" />' +
+    '<input name="note[longitude]" type="hidden" value="' + marker.position.lng() + '" />' +
+    '<input name="authenticity_token" type="hidden" value=' + MAPKEEP.authToken + ' />' +
+    '<input name="note[user_id]" type="hidden" value=' + MAPKEEP.userId + ' />' +
+    '<button class="button tiny right" type="submit" data-remote="true">Save</button>' +
+    '</form>'
+  });
+
   google.maps.event.addListener(marker, 'click', function() {
     infowindow.open(MAPKEEP.map, marker);
   });
