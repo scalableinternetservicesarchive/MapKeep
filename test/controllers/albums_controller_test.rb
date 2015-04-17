46a9ec49b594c5fcd17e2fa3a66c8b043bc89eb6
Tania DePasquale <rawrtan@gmail.com>
2015-04-17 15:38:33 -0700

    Add tests for album and note models

--- test/controllers/albums_controller_test.rb
+++ test/controllers/albums_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class AlbumsControllerTest < ActionController::TestCase
   setup do
-    @album = albums(:one)
+    @album = albums(:all_fields)
     sign_in users(:one)
   end
 
