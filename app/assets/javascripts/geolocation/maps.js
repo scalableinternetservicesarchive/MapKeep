60841d44683acfb53fa2bf7280fa1e4aa78d37e1
Tania DePasquale <rawrtan@gmail.com>
2015-04-18 18:59:33 -0700

    Add better note creation form logistics, still needs edit

--- app/assets/javascripts/geolocation/maps.js
+++ app/assets/javascripts/geolocation/maps.js
@@ -1,5 +1,6 @@
 var MAPKEEP = MAPKEEP || {};
-MAPKEEP.infoWindows = [];
+MAPKEEP.ct = 0;
+MAPKEEP.lastWindow = null;
 
 MAPKEEP.init = function(notes, auth) {
   MAPKEEP.notes = notes;
@@ -34,14 +35,11 @@ MAPKEEP.initMap = function() {
 
 MAPKEEP.addInfoWindowToNote = function(note, marker) {
   var infoWindow = new google.maps.InfoWindow({
-    content:  '<h3>' + note.title + '</h3>' + note.body + '<br/>' +
-              '<button class="button tiny right">Edit</button>'
+    content:  MAPKEEP.createNoteForm(marker, true, note)
   });
-  MAPKEEP.infoWindows.push(infoWindow);
 
-  google.maps.event.addListener(marker, 'click', function() {
-    infoWindow.open(MAPKEEP.map, marker);
-  });
+  google.maps.event.addListener(marker, 'click',
+    MAPKEEP.openWindow(infoWindow, marker));
 };
 
 MAPKEEP.dropPin = function() {
@@ -53,40 +51,70 @@ MAPKEEP.dropPin = function() {
   });
 
   var infoWindow = new google.maps.InfoWindow({
-    content:
-    '<form class="new_note" action="/notes" id="i' + MAPKEEP.infoWindows.length + '"' +
-      'accept-charset="UTF-8" method="post" data-remote="true">' +
-    '<label for="note_title">Title</label><input type="text" name="note[title]" id="note_title" /><br/>' +
-    '<label for="note_body">Body</label><textarea rows="5" name="note[body]" id="note_body"></textarea>' +
-    '<input name="note[latitude]" type="hidden" value="' + marker.position.lat() + '" />' +
-    '<input name="note[longitude]" type="hidden" value="' + marker.position.lng() + '" />' +
-    '<input name="authenticity_token" type="hidden" value=' + MAPKEEP.authToken + ' />' +
-    '<input name="form_id" type="hidden" value=i' + MAPKEEP.infoWindows.length + ' />' +
-    '<button class="button tiny right" type="submit">Save</button>' +
-    '</form>'
+    content: MAPKEEP.createNoteForm(marker, false)
+  });
+
+  // Cancel note if user closes info window before saving
+  google.maps.event.addListener(infoWindow,'closeclick', function(){
+    marker.setMap(null);
   });
 
   // Show info window after pin drops down
-  setTimeout(function() {
-    infoWindow.open(MAPKEEP.map, marker);
-  }, 500);
+  setTimeout(MAPKEEP.openWindow(infoWindow, marker), 500);
 
   // Open info window on click
-  google.maps.event.addListener(marker, 'click', function() {
-    infoWindow.open(MAPKEEP.map, marker);
-  });
+  google.maps.event.addListener(marker, 'click',
+    MAPKEEP.openWindow(infoWindow, marker));
 
   // Update coords on pin drag
-  var len = MAPKEEP.infoWindows.length;
+  var len = MAPKEEP.ct++;
   google.maps.event.addListener(marker, 'dragend', function() {
     var form = $('#i' + len);
     form.find('input[name=note\\[latitude\\]]').val(marker.position.lat());
     form.find('input[name=note\\[longitude\\]]').val(marker.position.lng());
   });
-
-  MAPKEEP.infoWindows.push(infoWindow);
 };
 
-MAPKEEP.toggleForm = function(formId) {
-
+// Close last info window and open new one
+MAPKEEP.openWindow = function(infoWindow, marker) {
+  return function() {
+    if (MAPKEEP.lastWindow) {
+      MAPKEEP.lastWindow.close();
+    }
+    infoWindow.open(MAPKEEP.map, marker);
+    MAPKEEP.lastWindow = infoWindow;
+  }
 };
+
+/**
+ * TODO: make jquery elements, edit button should toggle form, then become submit
+ * @param marker The note belongs to for coordinates
+ * @param readonly Whether or not this is a new note (readonly means it is an existing note)
+ * @param note Note to use title and body for if existing
+ * @returns {string}
+ */
+MAPKEEP.createNoteForm = function(marker, readonly, note) {
+  readonly = readonly ? 'readonly' : '';
+  var title = '<input type="text" name="note[title]" placeholder="Title" ' + readonly;
+  var body = '<textarea rows="4" name="note[body]" ' + readonly +
+             ' placeholder="Write anything you want about this location!">';
+  var buttonText = readonly ? 'Edit' : 'Save';
+  var formAction = readonly ? '/notes/' + note.id : '/notes';
+  var patch = readonly ? '<input type="hidden" name="_method" value="patch">' : '';
+
+  if (readonly) {
+    title += ' value="' + note.title + '"';
+    body += note.body;
+  }
+
+  return  '<form class="new_note" action="' + formAction + '" id="i' + MAPKEEP.ct + '"' +
+            'accept-charset="UTF-8" method="post" data-remote="true">' + patch +
+            title + ' /><br/>' + body + '</textarea>' +
+            '<input name="note[latitude]" type="hidden" value="' + marker.position.lat() + '"/>' +
+            '<input name="note[longitude]" type="hidden" value="' + marker.position.lng() + '"/>' +
+            '<input name="authenticity_token" type="hidden" value=' + MAPKEEP.authToken + ' />' +
+            '<input name="form_id" type="hidden" value=i' + MAPKEEP.ct + ' />' +
+            '<button class="button tiny right" type="submit">' + buttonText + '</button>' +
+          '</form>';
+};
+
