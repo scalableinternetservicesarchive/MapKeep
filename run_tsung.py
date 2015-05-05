5c8510a72c37dc6ba9667a0ac581f904b6c5250e
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2015-05-04 22:53:40 -0700

    Report the url of the tsung report

--- run_tsung.py
+++ run_tsung.py
@@ -15,6 +15,8 @@ def main():
 		print 'Not a valid tsung test case: {}'.format(args.filename)
 		sys.exit(2)
 
+	hostname = get_public_hostname()
+
 	# Start the script
 	try:
 		log_path = start_tsung(args.filename)
@@ -22,15 +24,22 @@ def main():
 		print e
 		sys.exit(1)
 
-	if compile_tsung_report(log_path) == 0
-		print 'Tsung run and compile successfully'
-	else:
-		print 'Tsung did not compile sucessfully'
+	compile_tsung_report(log_path)
+
+	report_file = os.path.join(log_path, 'report.html')
+	if not os.path.exists(report_file):
+		print 'Report file {} does not exist.'
+				'Tsung failed to compile'.format(report_file)
+		sys.exit(1)
+
+	print 'Tsung succesfully ran. The report can be found at:'
+	print os.path.join(hostname, os.path.split(log_path)[1], 'report.html')
 
 def get_public_hostname():
 	""" Get the public hostname of the ec2 instance """
 	proc = Popen(['ec2-metadata', '--public-hostname'], stdout=PIPE)
 	hostname = proc.communicate()[0]
+	print hostname
 	return hostname.split()[1]
 
 def replace_tsung_server(file, hostname):
@@ -51,11 +60,11 @@ def start_tsung(filename):
 		raise RuntimeError("Tsung did not run successfully")
 	
 	# Remove the quotes and take only the directory name
+	print log_path
 	log_path = log_path.replace('"', '').split()[-1]
 	
 	if not os.path.exists(log_path):
 		raise OSError("Tsung log path does not exist: ".format())
-	
 	return log_path
 
 def compile_tsung_report(log_path):
