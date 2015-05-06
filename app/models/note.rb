4fcefc60e06e11dcd5061cdb96723fb851e22920
Tania DePasquale <rawrtan@gmail.com>
2015-05-05 19:38:39 -0700

    Force index use

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,34 +4,35 @@ class Note < ActiveRecord::Base
   has_many :albums, :through => :collections
   before_save :set_point
   before_update :set_point
+  validates :title, :body, :user_id, presence: true
+  validates :latitude, :longitude, :user_id, numericality: true
 
-  # By default, use the GEOS implementation for spatial columns.
+  # By default, use the GEOS implementation for spatial columns
   self.rgeo_factory_generator = RGeo::Geos.method(:factory)
 
   # But use a geographic implementation for the :latlon column.
   set_rgeo_factory_for_column(:latlon, RGeo::Geographic.spherical_factory)
 
-  validates :title, :body, :user_id, presence: true
-  validates :latitude, :longitude, :user_id, numericality: true
-
+  # Uses lat and lng to create and set point within note
   def set_point
     self[:latlon] = RGeo::Geographic.spherical_factory(:srid => 4326)
                         .point(self[:longitude], self[:latitude])
   end
 
-  # Finds all public and user's notes close to location to degree
+  # Finds all public and user's notes close to location to certain degree
   def Note.find_by_proximity(location, degree, current_user)
     linestring_text = get_linestring_text(location, degree)
     Note.find_by_sql("
             SELECT *
             FROM notes
+            FORCE INDEX (index_notes_on_latlon)
             WHERE
               ((private=false AND user_id!=#{current_user.id}) OR
               user_id=#{current_user.id}) AND
               MBRContains(GeomFromText( '#{linestring_text}' ), notes.latlon)")
   end
 
-  # Gets text representation of linestring centered around user location
+  # Gets text representation of linestring centered around location
   def Note.get_linestring_text(location, degree)
     "LINESTRING(#{location.lng - degree} #{location.lat - degree},
                 #{location.lng + degree} #{location.lat + degree})"
