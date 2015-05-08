5165ba26b42252efcd5d1f622ca6261dbd221a15
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2015-05-08 01:30:51 -0700

    Add faker and some potential seeding data for performance testing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+num_users = 99
+num_users.times do |n|
+	u = User.new({
+		email: 'example_#{n+1}@example.com',
+		password: 'password',
+		password_confirmation: 'password'
+		})
+	u.save
+end
+
+1000.times do |n|
+	title = Faker::Lorem.sentence
+	body = Faker::Lorem.paragraph
+	lat = Faker::Address.latitude
+	long = Faker::Address.longitude
+	user_id = 1
+
+	Note.create(
+		title: title,
+		body: body,
+		user_id: 1,
+		latitude: lat,
+		longitude: long
+		)
+end
