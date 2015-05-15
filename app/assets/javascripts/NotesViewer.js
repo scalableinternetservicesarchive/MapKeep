0fcb079666a48866cd9b3ebf39313fc9b00d6c4c
Michael Xiang <michael.xiang.ca@gmail.com>
2015-05-15 00:23:47 -0700

    Added form to popup. Refactored a bit. Not yet functional.

--- app/assets/javascripts/NotesViewer.js
+++ app/assets/javascripts/NotesViewer.js
@@ -1,26 +1,94 @@
 var mapkeep = mapkeep || {};
 
-//mapkeep.NotesViewer.prototype = new mapkeep.App();
+var editModal = $('#editModal');
 
-var map = null;
+var notesApp = null;
 
+mapkeep.NotesViewer = function(userid, auth) {
+  /** The google map object */
+  this.map = null;
+  /** Last clicked marker */
+  this.curMarker = null;
+  /** The note form */
+  this.noteForm = $('#noteView');
+  /** The user id */
+  this.userid = userid;
+  /** The user's current location */
+  this.userLoc = null;
+  /** The current session token */
+  this.authToken = auth;
+
+  this.formManager = new mapkeep.FormManager(this, auth);
+};
+
+mapkeep.NotesViewer.prototype.initMap = function () {
+  // Initialize the map at UCLA
+  var mapOptions = {
+    zoom: 10,
+    mapTypeControl: false,
+    streetViewControl: false,
+    minZoom: 4,
+    center: new google.maps.LatLng(34.0722, -118.4441)  // center at UCLA
+  };
+
+  this.userLoc = mapOptions.center;
+  this.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+};
+
+mapkeep.NotesViewer.prototype.updateForm = function(note, albums) {
+  this.userLoc = new google.maps.LatLng(note.latitude, note.longitude);
+
+  this.noteForm.prop('action', '/notes/' + note.id);
+  this.noteForm.find('input[name=note\\[title\\]]')
+    .val(note.title);
+  this.noteForm.find('textarea')
+    .html(note.body);
+  this.noteForm.find('input[name=latitude]')
+    .val('' + note.latitude);
+  this.noteForm.find('input[name=longitude]')
+    .val('' + note.longitude);
+  this.noteForm.find('input[name=authenticity_token]')
+    .val('' + this.authToken);
+  this.noteForm.find('input[name=album_ids]')
+    .val(this.formManager.albumIdString(note.albums));
+  this.noteForm.find('#privateTrue')
+    .prop('defaultChecked', note.private);
+  this.noteForm.find('#privateFalse')
+    .prop('defaultChecked', !note.private);
+};
+
+mapkeep.NotesViewer.prototype.centerMap = function() {
+  google.maps.event.trigger(this.map, 'resize');
+  this.map.panTo(this.userLoc);
+};
+
+mapkeep.NotesViewer.prototype.updatePin = function() {
+  // Delete last pin
+  if (this.curMarker)
+    this.curMarker.setMap(null);
+
+  // Create and drop pin onto map
+  this.curMarker = new google.maps.Marker({
+    position: this.map.center,
+    map: this.map,
+    draggable: true,
+    animation: google.maps.Animation.DROP
+  });
+};
+
+
+/********* Document *********/
 $(document).ready(function() {
   $(document).foundation();
 
   // Set height the first time. Need to use absolute size to reveal Google Maps
   $('.reveal-modal').css('height', $('html').height() - 120 + 'px'); // 100+20px to keep modal effect visible
 
-  function initialize() {
-    var mapOptions = {
-      zoom: 10,
-      mapTypeControl: false,
-      streetViewControl: false,
-      center: new google.maps.LatLng(34.0722, -118.4441)  // center at UCLA
-    };
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  }
+  var auth = editModal.data('session');
+  var userid = editModal.data('userid');
+  notesApp = new mapkeep.NotesViewer(userid, auth);
 
-  google.maps.event.addDomListener(window, 'load', initialize);
+  google.maps.event.addDomListener(window, 'load', notesApp.initMap());
 });
 
 $(window).resize(function() {
@@ -28,32 +96,17 @@ $(window).resize(function() {
   $('.reveal-modal').css('height', $('html').height() - 120 + 'px');
 });
 
-var note = null;
-var albums = null;
-var app = null;
-
 $("a.reveal-link").click(function () {
-  $('#editModal').foundation('reveal', 'open');
-  note = $(this).data('note');
-  albums = $(this).data('albums');
+  var note = $(this).data('note');
+  var albums = $(this).data('albums');
+  notesApp.updateForm(note, albums);
 
-  /*
-  //TODO: move session token to html page
-  app = new mapkeep.App('<%= session[:_csrf_token] %>');
-  google.maps.event.addDomListener(window, 'load', function() {
-    app.init({lat: note.latitude, lng: note.longitude}, [note], albums);
-  });
-  */
+  editModal.foundation('reveal', 'open');
+
 });
 
-
-$('#editModal').bind('opened.fndtn.reveal', function() {
-  google.maps.event.trigger(map, 'resize');
-});
-
-$('#editModal').on('opened.fndtn.reveal', function() {
-  var center = (note && note.latitude && note.longitude) ?
-    new google.maps.LatLng(note.latitude, note.longitude) :
-    new google.maps.LatLng(34.0722, -118.4441);
-  map.setCenter(center);
+editModal.bind('opened.fndtn.reveal', function() {
+  // Otherwise the map will be off center and the wrong size
+  notesApp.centerMap();
+  notesApp.updatePin();
 });
