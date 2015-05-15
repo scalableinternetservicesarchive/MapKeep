5fac1803cc14d6dec69e97d263da9e7cf1f6a3d7
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2015-05-14 23:44:32 -0700

    Authenticate as random user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :random_auth
+
+  def random_auth
+  	# This value should be similar to the seed values
+  	max_users = 25
+  	rng = Random.new
+  	user_id = rng.rand(max_users)
+	user = User.find(user_id)
+	sign_in user
+  end
 end
