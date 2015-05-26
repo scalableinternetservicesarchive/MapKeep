abee3380085872e1932dc3965b714d95bc65598f
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2015-05-25 20:31:23 -0700

    Revert "Add tsung tests: main page and notes listing"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :random_auth
-
-  def random_auth
-  	user = User.where('email LIKE ?', '%example_%').all.sample
-  	sign_in user
-  end
+  before_action :authenticate_user!
 end
